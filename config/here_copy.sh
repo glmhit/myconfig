3d5833ef3e838d697672e64d02dc1cdbb145e285
glmhit <glmhit@126.com>
2018-04-19 10:18:33 +0800

    add haroopad.json cpplint.py

--- config/here_copy.sh
+++ config/here_copy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp -v ~/.zshrc ./
 cp -v ~/.vimrc.local ./
+cp -v ~/cpplint.py ./
+cp -v ~/Haroopad.json ./
 
 echo 'update finish'
--- config/setUbuntu.sh
+++ config/setUbuntu.sh
@@ -2,6 +2,9 @@
 cp -v ./.zshrc ~/.zshrc 
 cp -v ./.vimrc.local  ~/.vimrc.local 
 
+cp -v ./cpplint.py ./  ~/
+cp -v ./Haroopad.json ~/
+
 #ssh-keygen -t rsa -C "mail@126.com"
 #ssh-keygen -t rsa -C "mail@robosense.cn"
 echo 'install finish'
