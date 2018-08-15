25f0ec7e64ec1c1d6d0c5577668c9e8b02b684e6
glmhit <glmhit@126.com>
2018-08-15 15:04:30 +0800

    feat:添加git clang-format hook

--- config/here_copy.sh
+++ config/here_copy.sh
@@ -6,6 +6,9 @@ cp -v ~/cpplint.py ./
 cp -v ~/Haroopad.json ./
 cp -v ~/.clang-format ./
 cp -v ~/.gitconfig ./
+cp -v ~/pre-commit ./
+cp -v ~/pre-commit ./
+cp -v ~/.gitmessage.txt
 cp -v ~/.matlab/R2018a/*.settings ./matlab 
 cp -v ~/.matlab/R2018a/matlab.prf ./matlab 
 cp -v ~/.zotero/zotero/sgfeem6o.default/prefs.js ./zetero.prefs.js
