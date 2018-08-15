ff9f4082c095775efe422ca07a6c510b2cb6b6f2
glmhit <glmhit@126.com>
2018-08-15 15:31:53 +0800

    feat:修改保存路径

--- config/here_copy.sh
+++ config/here_copy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-cp -v ~/.zshrc ./
-cp -v ~/.bshrc ./
-cp -v ~/.vimrc.local ./
-cp -v ~/cpplint.py ./
-cp -v ~/Haroopad.json ./
-cp -v ~/.clang-format ./
-cp -v ~/.gitconfig ./
-cp -v ~/pre-commit ./
-cp -v ~/pre-commit ./
-cp -v ~/.gitmessage.txt
-cp -v ~/.matlab/R2018a/*.settings ./matlab 
-cp -v ~/.matlab/R2018a/matlab.prf ./matlab 
-cp -v ~/.zotero/zotero/sgfeem6o.default/prefs.js ./zetero.prefs.js
+basepath=$(cd `dirname $0`; pwd)
+echo $basepath
+cp -v ~/.zshrc $basepath
+cp -v ~/.bshrc $basepath
+cp -v ~/.vimrc.local $basepath
+cp -v ~/cpplint.py $basepath
+cp -v ~/Haroopad.json $basepath
+cp -v ~/.clang-format $basepath
+cp -v ~/.gitconfig $basepath
+cp -v ~/pre-commit $basepath
+cp -v ~/pre-commit $basepath
+cp -v ~/.gitmessage.txt $basepath
+cp -v ~/.matlab/R2018a/*.settings $basepath/matlab/ 
+cp -v ~/.matlab/R2018a/matlab.prf $basepath/matlab/ 
+cp -v ~/.zotero/zotero/sgfeem6o.default/prefs.js $basepath/zetero/
 echo 'update finish'
